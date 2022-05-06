7a6de806b403b6d547b5dfbba3023d9adee0cfe8
Ľuboš Mudrák <lubosmudrak93@gmail.com>
2022-05-06 19:48:43 +0200

    basic application loop

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,15 @@
+mod parsing;
+
 fn main() {
+    let mut exit_command = false;
+    println!("Wellcome to Calc New Roman!\nType exit to quit this application.");
+
+    while exit_command == false{
+        let user_input = parsing::get_user_input();
+        match &user_input as &str {
+            "exit" => (exit_command = true),
+            _ => parsing::parse_user_input()
+        }
+    }
+    println!("Thank you for using Calc New Roman!");
 }
