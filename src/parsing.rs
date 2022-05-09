2dbcd8492baef4734d4f07f4ef73d6eb67527ad2
Ľuboš Mudrák <lubosmudrak93@gmail.com>
2022-05-09 08:37:35 +0200

    tests to check if roman numbers are correctly converted

--- src/parsing.rs
+++ src/parsing.rs
@@ -32,3 +32,101 @@ pub fn parse_user_input(user_input: String) -> Option<u16>
         Some(number_counter) //TODO: return None in case of invalid user input
 }
 
+/*
+    There are 4 basic principles for writing Roman numerals as listed below:
+
+    The letters I, X, C can be repeated thrice in succession. Additionally, L, V, D cannot be repeated or the number is considered to be invalid.
+    If a lower value digit is written to the left of a higher value digit, it is subtracted.
+    If a lower value digit is written to the right of a higher value digit, it is added.
+    Only I, X, and C can be used as subtractive numerals.
+*/
+
+#[test]
+fn parse_i(){
+    assert_eq!(parse_user_input("I".to_string()), Some(1));
+}
+
+#[test]
+fn parse_ii(){
+    assert_eq!(parse_user_input("iI".to_string()), Some(2));
+}
+
+#[test]
+fn parse_iiii(){
+    assert_eq!(parse_user_input("iiii".to_string()), Option::None);
+}
+
+#[test]
+fn parse_iv(){
+    assert_eq!(parse_user_input("IV".to_string()),Some(4));
+}
+
+#[test]
+fn parse_v(){
+    assert_eq!(parse_user_input("V".to_string()), Some(5));
+}
+
+#[test]
+fn parse_x(){
+    assert_eq!(parse_user_input("X".to_string()), Some(10));
+}
+
+#[test]
+fn parse_lxiii(){
+    assert_eq!(parse_user_input("LXIII".to_string()), Some(63));
+}
+
+#[test]
+fn parse_lxiv(){
+    assert_eq!(parse_user_input("LXIV".to_string()), Some(64));
+}
+
+#[test]
+fn parse_xxxx(){
+    assert_eq!(parse_user_input("XXXX".to_string()), Option::None);
+}
+
+#[test]
+fn parse_xxxxi(){
+    assert_eq!(parse_user_input("XXXXI".to_string()), Option::None);
+}
+
+#[test]
+fn parse_xxll(){
+    assert_eq!(parse_user_input("XXLL".to_string()), Option::None);
+}
+
+#[test]
+fn parse_llc(){
+    assert_eq!(parse_user_input("LLC".to_string()), Option::None);
+}
+
+#[test]
+fn parse_lc(){
+    assert_eq!(parse_user_input("LC".to_string()), Option::None);
+}
+
+#[test]
+fn parse_ilc(){
+    assert_eq!(parse_user_input("ILC".to_string()), Option::None);
+}
+
+#[test]
+fn parse_mmmx(){
+    assert_eq!(parse_user_input("MMMX".to_string()), Some(3010));
+}
+
+#[test]
+fn parse_mmmcmxcix(){
+    assert_eq!(parse_user_input("MMMCMXCIX".to_string()), Some(3999));
+}
+
+#[test]
+fn parse_lxxxix(){
+    assert_eq!(parse_user_input("LXXXIX".to_string()), Some(89));
+}
+
+#[test]
+fn parse_lxxxiv(){
+    assert_eq!(parse_user_input("LXXXIV".to_string()), Some(84));
+}
