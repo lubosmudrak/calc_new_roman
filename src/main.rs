565e6cac202bda3f94392d455d0593583b71736d
Ľuboš Mudrák <lubosmudrak93@gmail.com>
2022-05-18 16:48:35 +0200

    cleanup+fix typo

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod parsing;
 
 fn main() {
     let mut exit_command = false;
-    println!("Wellcome to Calc New Roman!\nType HELP \"help\" to display instruction manual ");
+    println!("Wellcome to Calc New Roman!\nType \"help\" to display instruction manual ");
 
     while exit_command == false{
         let user_input = parsing::get_user_input();
@@ -11,12 +11,14 @@ fn main() {
             Some(x) => println!("Result: {}",x),
             None => {
                 match &user_input as &str {
-                    "exit" => (exit_command = true),
+                    "exit" => {
+                        exit_command = true;
+                        println!("Thank you for using Calc New Roman!");
+                    },
                     "help" => (println!("TODO: documentation...\n")),
                     _ => (println!("ERROR: invalid input!"))
                 }
             }
         }
     }
-    println!("Thank you for using Calc New Roman!");
 }
